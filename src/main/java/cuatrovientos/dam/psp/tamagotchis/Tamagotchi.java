63dbadc497acc39fad6d4accc9142a7306b97099
Julen <julenoa58@gmail.com>
2025-11-02 16:47:34 +0100

    atributos y funciones básicas en tamagotchi

--- src/main/java/cuatrovientos/dam/psp/tamagotchis/Tamagotchi.java
+++ src/main/java/cuatrovientos/dam/psp/tamagotchis/Tamagotchi.java
@@ -1,5 +1,28 @@
 package cuatrovientos.dam.psp.tamagotchis;
 
-public class Tamagotchi {
+public class Tamagotchi implements Runnable {
+	private String nombre;
+	private boolean vivo = true;
+	private boolean ocupado = false;
+	private long inicioVida;
+    private int suciedad = 0;
+    
+    public Tamagotchi(String nombre) {
+        this.nombre = nombre;
+        this.inicioVida = System.currentTimeMillis();
+    }
+    
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+	public boolean estaVivo() {
+		return vivo;
+	}
 
 }
