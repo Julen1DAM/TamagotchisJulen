457e0a99b16da2cf9a7e706ae2bd04af0e62270b
Julen <julenoa58@gmail.com>
2025-11-02 17:05:00 +0100

    funcion comer

--- src/main/java/cuatrovientos/dam/psp/tamagotchis/Tamagotchi.java
+++ src/main/java/cuatrovientos/dam/psp/tamagotchis/Tamagotchi.java
@@ -1,15 +1,19 @@
 package cuatrovientos.dam.psp.tamagotchis;
 
+import java.util.Random;
+
 public class Tamagotchi implements Runnable {
 	private String nombre;
 	private boolean vivo = true;
 	private boolean ocupado = false;
 	private long inicioVida;
     private int suciedad = 0;
+    private long velocidadComer;
     
     public Tamagotchi(String nombre) {
         this.nombre = nombre;
         this.inicioVida = System.currentTimeMillis();
+        this.velocidadComer = (long)(Math.random() * 3001);
     }
     
 	@Override
@@ -23,6 +27,22 @@ public class Tamagotchi implements Runnable {
 	}
 	public boolean estaVivo() {
 		return vivo;
+	}
+	
+	public void comer() {
+		if(!vivo) {
+			System.out.println(nombre + " no puede comer, está muerto");
+		}else if(ocupado){
+			System.out.println(nombre + " no puede comer, está ocupado");
+		}
+		ocupado = true;
+		System.out.println("¡" + nombre + " ha empezado a comer!");
+		try {
+			Thread.sleep(velocidadComer);
+		} catch (InterruptedException e) {
+			System.out.println("Ha terminado de comer");
+			ocupado = false;
+		}
 	}
 
 }
